1c380cf6ceb65a07badcdc13979b5164c42742dd
Dmitry <113033685+Flyer-DM@users.noreply.github.com>
2022-09-29 13:52:33 +0300

    added client "exit" function

--- pythonProject_clientserver/venv/client.py
+++ pythonProject_clientserver/venv/client.py
@@ -4,11 +4,12 @@ sock = socket.socket()
 sock.setblocking(1)
 sock.connect(('localhost', 9090))
 
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
+while True:
+    message = input('Введите сообщение для сервера: ')
+    print("Отправление сообщения серверу...")
+    sock.send(message.encode())
+    print("Сообщение успешно доставлено!")
+    if message == 'exit':
+        break
 
 sock.close()
-
-print(data.decode())
