ab3ff4ff495b534aa90cf9ae6c79c7e30490d361
Dmitry <113033685+Flyer-DM@users.noreply.github.com>
2022-09-29 14:24:52 +0300

    constant listening by server
    Server will wait for one client to connect until the connected client send message "shutdown" to close the server

--- pythonProject_clientserver/venv/server.py
+++ pythonProject_clientserver/venv/server.py
@@ -2,17 +2,29 @@ import socket
 
 sock = socket.socket()  # создание соккета
 sock.bind(('', 9090))  # хост общедоступный и порт
-sock.listen(1)  # включаем режим прослушивания
-print("Включён режим прослушивания.")
-conn, addr = sock.accept()  # получаем новый соккет и адрес клиента
-print("Успешное соединение клиента:", addr[0])
+
+flag1, breaker = True, False
 
 while True:
-    data = conn.recv(1024)  # читаем данные по 1 Кб
-    message = data.decode()
-    print("Полученное сообщение:", message)
-    if message == 'exit':
-        print("Отключение от сервера по запросу клиента.")
-        break
+    sock.listen(1)  # включаем режим прослушивания
+    if flag1:
+        print("Включён режим прослушивания.")
+        flag1 = not flag1
 
-conn.close()  # закрываем соединение
+    conn, addr = sock.accept()  # получаем новый соккет и адрес клиента
+    print("Успешное соединение клиента:", addr[0])
+
+    while True:
+        data = conn.recv(1024)  # читаем данные по 1 Кб
+        message = data.decode()
+        if not message:
+            print('Отключение клиента:', addr[0])
+            break
+        print("Полученное сообщение:", message)
+        if message == 'shutdown':
+            breaker = not breaker
+            break
+    if breaker:
+        print("Отключение сервера.")
+        conn.close()  # закрываем соединение
+        break
