b9928805fc40af011ec5008398850d15ec459884
Dmitry <113033685+Flyer-DM@users.noreply.github.com>
2022-09-29 13:53:10 +0300

    added "exit" from client function

--- pythonProject_clientserver/venv/server.py
+++ pythonProject_clientserver/venv/server.py
@@ -3,18 +3,16 @@ import socket
 sock = socket.socket()  # создание соккета
 sock.bind(('', 9090))  # хост общедоступный и порт
 sock.listen(1)  # включаем режим прослушивания
+print("Включён режим прослушивания.")
 conn, addr = sock.accept()  # получаем новый соккет и адрес клиента
-print(addr)
-
-msg = ''
+print("Успешное соединение клиента:", addr[0])
 
 while True:
-    data = conn.recv(1024)  # читаем данные по 1024 Кб
-    if not data:  # как только клент перестаёт отправлять данные
+    data = conn.recv(1024)  # читаем данные по 1 Кб
+    message = data.decode()
+    print("Полученное сообщение:", message)
+    if message == 'exit':
+        print("Отключение от сервера по запросу клиента.")
         break
-    msg += data.decode()
-    conn.send(data)  # возвращаем сообщение клиента
-
-print(msg)
 
 conn.close()  # закрываем соединение
