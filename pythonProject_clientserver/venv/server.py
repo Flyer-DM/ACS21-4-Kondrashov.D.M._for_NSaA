4c2e8f38356201a15d73421202cb5b073beecdaf
Dmitry <113033685+Flyer-DM@users.noreply.github.com>
2022-09-29 15:24:57 +0300

    created log.txt file
    now while working server writes all processes occuring with it into log.txt file

--- pythonProject_clientserver/venv/server.py
+++ pythonProject_clientserver/venv/server.py
@@ -1,4 +1,7 @@
 import socket
+import datetime
+
+f = open('log.txt', 'w')
 
 sock = socket.socket()  # создание соккета
 sock.bind(('', 9090))  # хост общедоступный и порт
@@ -9,22 +12,30 @@ while True:
     sock.listen(1)  # включаем режим прослушивания
     if flag1:
         print("Включён режим прослушивания.")
+        f.write("Включён режим прослушивания." + ' | ' + str(datetime.datetime.now()) + '\n')
         flag1 = not flag1
 
     conn, addr = sock.accept()  # получаем новый соккет и адрес клиента
     print("Успешное соединение клиента:", addr[0])
+    f.write("Успешное соединение клиента:" + addr[0] + ' | ' + str(datetime.datetime.now()) + '\n')
+    conn.send("Слышу вас!".encode())
 
     while True:
         data = conn.recv(1024)  # читаем данные по 1 Кб
         message = data.decode()
         if not message:
             print('Отключение клиента:', addr[0])
+            f.write("Отключение клиента:" + addr[0] + ' | ' + str(datetime.datetime.now()) + '\n')
             break
-        print("Полученное сообщение:", message)
+        print("Полученное сообщение : ", message)
+        f.write("Полученное сообщение : " + message + ' | ' + str(datetime.datetime.now()) + '\n')
         if message == 'shutdown':
             breaker = not breaker
             break
     if breaker:
         print("Отключение сервера.")
+        f.write("Отключение сервера." + ' | ' + str(datetime.datetime.now()) + '\n')
         conn.close()  # закрываем соединение
         break
+
+f.close()
