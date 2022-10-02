8773f7b039f2d251302fcae1c17fbe0cb5d59217
Dmitry <113033685+Flyer-DM@users.noreply.github.com>
2022-10-02 20:56:25 +0300

    functions to send and receive messages for server

--- pythonProject_clientserver/venv/server.py
+++ pythonProject_clientserver/venv/server.py
@@ -2,8 +2,23 @@ import socket
 import datetime
 import random
 import hashlib
-
 f = open('log.txt', 'w')
+
+
+def logger(message: str) -> None:
+    f.write(message + ' | ' + str(datetime.datetime.now()) + '\n')
+    print(message)
+
+
+def sender(sock: socket.socket, message: str) -> None:
+    sock.send(bytearray(f'{len(message)}~{message}'.encode()))
+
+
+def reciever(sock: socket.socket) -> str:
+    message = sock.recv(1024).decode()
+    print(f"Получено сообщение длины: {message[:message.index('~')]}")
+    return message[message.index('~') + 1:]
+
 
 sock = socket.socket()  # создание сокета
 average_port = 9090
@@ -17,69 +32,62 @@ while True:
         average_port = random.randint(1024, 65535)
 
 flag1, breaker, attempts = True, False, 3
-
 while True:
     sock.listen(1)  # включаем режим прослушивания
     if flag1:
-        print("Включён режим прослушивания.")
-        f.write("Включён режим прослушивания." + ' | ' + str(datetime.datetime.now()) + '\n')
+        logger("Включён режим прослушивания.")
         flag1 = not flag1
-
     conn, addr = sock.accept()  # получаем новый сокет и адрес клиента
-    with open('clinets.txt', 'a+') as clients_list:
+
+    with open('clients.txt', 'a+') as clients_list:
         clients_list.seek(0, 0)
         #  поиск имеющегося клиента
         for line in clients_list:
             if addr[0] in line:
-                f.write("Подключение известного клиента: " + addr[0] + ' | ' + str(datetime.datetime.now()) + '\n')
-                conn.send('known'.encode())
-                conn.send(("Здравствуйте, " + line[line.index('|') + 1:line.rindex('|')]).encode())
+                logger("Подключение известного клиента: " + addr[0])
+                sender(conn, 'known')
+                sender(conn, "Здравствуйте, " + line[line.index('|') + 1:line.rindex('|')] + "!")
                 while True:
-                    conn.send("Введите свой пароль: ".encode())
-                    f.write("Запрос пароля клиента: " + addr[0] + ' | ' + str(datetime.datetime.now()) + '\n')
-                    password = conn.recv(1024).decode()
+                    sender(conn, "Введите свой пароль: ")
+                    logger("Запрос пароля клиента: " + addr[0])
+                    password = reciever(conn)
                     if line[line.rindex('|') + 1:] == hashlib.md5(password.encode()).hexdigest():
-                        conn.send("true".encode())
-                        conn.send("Вы подтвердили свой пароль!".encode())
-                        f.write("Клиент подтвердил свой пароль" + ' | ' + str(datetime.datetime.now()) + '\n')
+                        sender(conn, "true")
+                        sender(conn, "Вы подтвердили свой пароль!")
+                        logger("Клиент подтвердил свой пароль")
                         break
                     else:
                         attempts -= 1
                     if attempts == 0:
-                        conn.send('false'.encode())
-                        conn.send("Количество попыток исчерпано!".encode())
+                        sender(conn, 'false')
+                        sender(conn, "Количество попыток исчерпано!")
                         break
                 break
         else:
             #  запись нового клиента с его паролем и именем
-            conn.send('new'.encode())  # флаг, означающий первое подключение клиента
-            conn.send("Вы незарегистрированный пользователь! Введите ваше имя: ".encode())  # 1 сообщение клиенту
-            name = conn.recv(1024).decode()  # 1 сообщение от клиента
-            conn.send("Введите пароль: ".encode())  # 2 сообщение клиенту
-            password = conn.recv(1024).decode()  # 2 сообщение от клиента
+            sender(conn, 'new')  # флаг, означающий первое подключение клиента
+            sender(conn, "Вы незарегистрированный пользователь! Введите ваше имя: ")  # 1 сообщение клиенту
+            name = reciever(conn)  # 1 сообщение от клиента
+            sender(conn, "Введите пароль: ")  # 2 сообщение клиенту
+            password = reciever(conn)  # 2 сообщение от клиента
             password = hashlib.md5(password.encode()).hexdigest()  # хешерование пароля
             clients_list.write('\n' + addr[0] + '|' + name + '|' + password)
-            f.write("Запись нового клиента: " + addr[0] + ' | ' + str(datetime.datetime.now()) + '\n')
+            logger("Запись нового клиента: " + addr[0])
 
     if attempts != 0:
-        print("Успешное соединение клиента: " + addr[0])
-        f.write("Успешное соединение клиента: " + addr[0] + ' | ' + str(datetime.datetime.now()) + '\n')
+        logger("Успешное соединение клиента: " + addr[0])
 
         while True:
-            data = conn.recv(1024)  # читаем данные по 1 Кб
-            message = data.decode()  # декодирование битов в текст
+            message = reciever(conn)  # читаем данные по 1 Кб и декадируем в текст
             if not message:
-                print('Отключение клиента: ', addr[0])
-                f.write("Отключение клиента: " + addr[0] + ' | ' + str(datetime.datetime.now()) + '\n')
+                logger("Отключение клиента: " + addr[0])
                 break
-            print("Полученное сообщение :", message)
-            f.write("Полученное сообщение: " + message + ' | ' + str(datetime.datetime.now()) + '\n')
+            logger("Полученное сообщение: " + message)
             if message == 'shutdown':
                 breaker = not breaker
                 break
         if breaker:
-            print("Отключение сервера.")
-            f.write("Отключение сервера." + ' | ' + str(datetime.datetime.now()) + '\n')
+            logger("Отключение сервера.")
             conn.close()  # закрываем соединение
             break
 
